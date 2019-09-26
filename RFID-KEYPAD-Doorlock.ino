b6a5a51361f873971fedaad67f8c647a6a9a4311
Tharindu Dias <tkdias0@gmail.com>
2019-09-26 09:17:39 +0530

    Include required libraries

---
+++ RFID-KEYPAD-Doorlock.ino
@@ -0,0 +1,16 @@
+// Include required libraries
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+#include <Servo.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
