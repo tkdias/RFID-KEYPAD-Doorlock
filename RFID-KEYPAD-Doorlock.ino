63df8c8de9e0d4e3d4a55067393301424c18270d
Tharindu Dias <tkdias0@gmail.com>
2019-09-26 09:19:28 +0530

    Initializing I/O pins and libs

--- RFID-KEYPAD-Doorlock.ino
+++ RFID-KEYPAD-Doorlock.ino
@@ -5,6 +5,44 @@
 #include <Servo.h>
 #include <SPI.h>
 
+// Create instances
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+MFRC522 mfrc522(10, 9); // MFRC522 mfrc522(SS_PIN, RST_PIN)
+Servo sg90;
+
+// Initialize Pins for led's, servo and buzzer
+// Blue LED is connected to 5V
+constexpr uint8_t greenLed = 7;
+constexpr uint8_t redLed = 6;
+constexpr uint8_t servoPin = 8;
+constexpr uint8_t buzzerPin = 5;
+
+char initial_password[4] = {'1', '2', '3', '4'};  // Variable to store initial password
+String tagUID = "29 B9 ED 23";  // String to store UID of tag. Change it with your tag's UID
+char password[4];   // Variable to store users password
+boolean RFIDMode = true; // boolean to change modes
+char key_pressed = 0; // Variable to store incoming keys
+uint8_t i = 0;  // Variable used for counter
+
+// defining how many rows and columns our keypad have
+const byte rows = 4;
+const byte columns = 4;
+
+// Keypad pin map
+char hexaKeys[rows][columns] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+// Initializing pins for keypad
+byte row_pins[rows] = {A0, A1, A2, A3};
+byte column_pins[columns] = {2, 1, 0};
+
+// Create instance for keypad
+Keypad keypad_key = Keypad( makeKeymap(hexaKeys), row_pins, column_pins, rows, columns);
+
 void setup() {
   // put your setup code here, to run once:
 
